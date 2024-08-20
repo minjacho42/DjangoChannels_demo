972611f9693a5ac122c1441469caff0d354bab8f
minjacho42 <louis9982.dev@gmail.com>
2024-08-20 20:04:16 +0900

    feat: check collision duplicated compare remove

--- remote_game/game_objects/Paddle.py
+++ remote_game/game_objects/Paddle.py
@@ -17,6 +17,8 @@ class Paddle:
             self.y += self.dy
 
     def check_collision(self, ball, direction):
+        if not (self.x - ball.radius <= ball.x <= self.x + self.width + ball.radius):
+            return
         ballL = ball.x - ball.radius
         ballR = ball.x + ball.radius
         ballT = ball.y - ball.radius
@@ -27,28 +29,28 @@ class Paddle:
         padB = self.y + self.height
 
         # 좌측 패들의 우측면 충돌 판정
-        if direction == 'L' and ballL < padR and ballL > padL:
+        if direction == 'L' and padR > ballL > padL:
             # 우측면 충돌
             if ball.dx <= 0:
                 # 우상단 -> 좌하단 접근 시
-                if ball.dy <= 0 and ballB >= padT and ballB < self.y + self.height / 2:
+                if ball.dy <= 0 and padT <= ballB < self.y + self.height / 2:
                     ball.handle_collision('L', self.dy)
                 # 우하단 -> 좌상단 접근 시
-                elif ball.dy >= 0 and ballT <= padB and ballT > self.y + self.height / 2:
+                elif ball.dy >= 0 and padB >= ballT > self.y + self.height / 2:
                     ball.handle_collision('L', self.dy)
                 # 패들과 겹치는 경우
                 elif ballB >= padT and ballT <= padB:
                     ball.handle_collision('L', self.dy)
 
         # 우측 패들의 좌측면 충돌 판정
-        elif direction == 'R' and ballR > padL and ballR < padR:
+        elif direction == 'R' and padL < ballR < padR:
             # 좌측면 충돌
             if ball.dx >= 0:
                 # 좌상단 -> 우하단 접근 시
-                if ball.dy <= 0 and ballB >= padT and ballB < self.y + self.height / 2:
+                if ball.dy <= 0 and padT <= ballB < self.y + self.height / 2:
                     ball.handle_collision('R', self.dy)
                 # 좌하단 -> 우상단 접근 시
-                elif ball.dy >= 0 and ballT <= padB and ballT > self.y + self.height / 2:
+                elif ball.dy >= 0 and padB >= ballT > self.y + self.height / 2:
                     ball.handle_collision('R', self.dy)
                 # 패들과 겹치는 경우
                 elif ballB >= padT and ballT <= padB:
